5af47e33da27e2e41773d89ea97f2db7cb908176
NestorLlanos <nestordllanos@gmail.com>
2024-03-19 11:38:37 -0300

    modificando Navbar

--- src/components/Nabvar/Navbar.jsx
+++ src/components/Nabvar/Navbar.jsx
@@ -2,27 +2,27 @@ import CartWidget from "../CartWidget/CartWidget"
 import classes from "./Navbar.module.css"
 import {Link } from "react-router-dom"
 import logo from "../../assets/logo Tortita con manteca.png"
+import {FaBars} from "react-icons/fa6";
 
 
 const Navbar = () =>{
     return(
         <header className={classes.header}>
+            <div className={classes.container}></div>
             <Link to='/' >               
                <img src={logo} alt="Logo Tortita con Manteca" className={classes.logo}/>
             </Link>
 
             <nav className={classes.nav}>
-                <Link to ='/category/tortas' className={classes.a}>Tortas</Link>
-                <Link to='/category/alfajores' className={classes.a}>Alfajores</Link>
-                <Link to='/category/tartas' className={classes.a}>Tartas</Link>
-                <Link to='/category/box' className={classes.a}>Box</Link>
+                <Link to ='/category/tortas' className={classes.navLink}>Tortas</Link>
+                <Link to='/category/alfajores' className={classes.navLink}>Alfajores</Link>
+                <Link to='/category/tartas' className={classes.navLink}>Tartas</Link>
+                <Link to='/category/box' className={classes.navLink}>Box</Link>
             </nav>
             
             <CartWidget/> 
-            
-                
-            
-            
+            <button className={classes.mobileIcon}></button>     
+            <FaBars />
         </header>
     )
 }
